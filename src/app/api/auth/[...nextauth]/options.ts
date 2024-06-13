d13f1627dfba47670c768a5438c63569c5bcae94
Frank Ezene <ezenefrankc@gmail.com>
2024-06-13 14:19:59 +0100

    feat: create options.ts and page.tsx

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,16 @@
+import Auth0Provider from "next-auth/providers/auth0";
+
+const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    // !!! Should be stored in .env file.
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID as string,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+      issuer: process.env.AUTH0_ISSUER,
+    }),
+  ],
+  secret: `UItTuD1HcGXIj8ZfHUswhYdNd40Lc325R8VlxQPUoR0=`,
+};
+
+export default authOptions;
