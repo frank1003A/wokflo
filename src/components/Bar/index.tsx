08336ddc3c9442ffb571741fae8a8a1f27a2ff55
Frank Ezene <ezenefrankc@gmail.com>
2024-06-13 23:05:08 +0100

    update 1 file and create 2 files

--- src/components/Bar/index.tsx
+++ src/components/Bar/index.tsx
@@ -1,19 +1,65 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@components/ui/accordion";
+import { Separator } from "@components/ui/separator";
+import { useMenu } from "@refinedev/core";
 import Link from "next/link";
 
 const LeftBar = () => {
-  const List = ["test"];
+  const { menuItems, selectedKey, defaultOpenKeys } = useMenu();
+
   return (
-    <div className="max-w-[280px] w-full h-full bg-[#161719] px-5">
+    <div className="max-w-[280px] w-full h-full bg-[#161719]">
       <Link href={"/"}>
         <h1 className="font-bold">Logo</h1>
       </Link>
 
       <ul className="flex flex-col">
-        {List?.map((list, index) => {
-          return <li key={list}>{list}</li>;
+        {menuItems.map((item, index) => {
+          return (
+            <li key={item.name}>
+              <Link
+                href={item.route as string}
+                className="transition-all hover:bg-primary_hover px-6 p-3 flex items-center justify-start gap-4 text-sm capitalize text-white font-bold w-full"
+              >
+                <span className="text-zinc-500 h-6 w-6"> {item.icon}</span>
+                {item.name}
+              </Link>
+            </li>
+          );
         })}
       </ul>
+      <Separator className="border border-zinc-500" />
+      {/** Favorite */}
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger className="no-underline hover:bg-primary_hover text-base capitalize text-zinc-500 px-6">
+            <div className="transition-all flex items-center justify-start gap-4 font-bold w-full">
+              Favorite
+            </div>
+          </AccordionTrigger>
+          <AccordionContent>
+            <ul className="flex flex-col">
+              <li className="flex items-center justify-between transition-all hover:bg-primary_hover px-5 p-3 gap-4 text-sm capitalize text-white font-bold w-full">
+                <Link
+                  className="flex items-center justify-start gap-4"
+                  href={"/favorite/3232424"}
+                >
+                  <div className="text-zinc-500 w-6 h-6  rounded-md bg-sky-400"></div>
+                  StrataScrach
+                </Link>
 
+                <button className="h-6 w-6 text-2xl bg-none flex items-center justify-center">
+                  ...
+                </button>
+              </li>
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
       {/**
        * <Link href={"/"}>
         <h1 className="font-bold">Logo</h1>
