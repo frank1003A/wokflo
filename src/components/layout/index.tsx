9e019409e597bfc8033493f553b9166d7d146cf0
Frank Ezene <ezenefrankc@gmail.com>
2024-06-14 10:48:13 +0100

    update src/components/layout/index.tsx and button.tsx

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,15 +1,27 @@
 "use client";
 
 import LeftBar from "@components/Bar";
+import useLayoutStore from "@zustand/store/layoutstore/store";
 import { PropsWithChildren } from "react";
 
 export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+  const { toggleCollapsed } = useLayoutStore((state) => state);
   return (
     <main className="flex w-full h-screen bg-[#161719]">
       <LeftBar />
-      <div className="content w-full bg-[#1f2125] rounded-tl-3xl border-l-2 border-t-2 border-[#33353b]">
+      <div className="content w-full h-full bg-[#1f2125] rounded-tl-3xl border-l-2 border-t-2 border-[#33353b]">
         {/**<Breadcrumb /> */}
-        <div>{children}</div>
+        {/**<div className="w-full flex p-3">
+          <Button
+            onClick={toggleCollapsed}
+            className="ml-auto lg:hidden text-white text-lg"
+            variant={"ghost"}
+            size={"icon"}
+          >
+            <Menu />
+          </Button>
+        </div> */}
+        {children}
       </div>
     </main>
   );
