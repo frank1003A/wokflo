0e2e955212d1938fdd6d82a6464f35fd08a5d143
Frank Ezene <ezenefrankc@gmail.com>
2024-06-19 10:15:09 +0100

    update 1 file and create 2 files

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,19 @@
-"use client ";
+"use client";
 import { useGetIdentity } from "@refinedev/core";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../assets/logo.png";
 
 const Home = () => {
-  const { data: identity } = useGetIdentity<{ email: string }>();
+  const { data: identity } = useGetIdentity<{ name: string; id: string }>();
 
-  return <span>{identity?.email}</span>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <Link href={"/"}>
+        <Image src={logo} alt="wokflo_logo" width={400} height={400} />
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
