0dc1df89bf098c2275a75d0ccc3b28d6f48d0d56
Frank Ezene <ezenefrankc@gmail.com>
2024-06-21 03:32:35 +0100

    update Task.tsx and accordion.tsx

--- src/components/Lists/Task.tsx
+++ src/components/Lists/Task.tsx
@@ -25,7 +25,7 @@ import { CSS } from "@dnd-kit/utilities";
 import { Project, Task } from "@prisma/client";
 import { AccordionItem } from "@radix-ui/react-accordion";
 import { HttpError, useList } from "@refinedev/core";
-import { EllipsisVertical, GripVertical } from "lucide-react";
+import { ClipboardList, EllipsisVertical, GripVertical } from "lucide-react";
 
 const SingleTask = ({ task, id }: { task: Task; id: string }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
@@ -50,7 +50,7 @@ const SingleTask = ({ task, id }: { task: Task; id: string }) => {
         icon={<GripVertical />}
         tip="Drag Task"
       />
-      <Checkbox className="ml-2 h-5 w-5" />
+      <Checkbox className="ml-2 h-5 w-5 bg-white border-none data-[state=checked]:bg-sky-600" />
       <div className="ml-4">
         <span>{task.title}</span>
       </div>
@@ -152,17 +152,27 @@ const TasksView = () => {
                 value={`item-${index + 1}`}
                 className="p-2 rounded-md w-full mb-4 border border-zinc-700 border-dashed"
               >
-                <AccordionTrigger className="bg-transparent border-zinc-700 py-2 px-2 mb-2 rounded-tl-md rounded-tr-md hover:*:no-underline">
-                  <div className="flex items-center gap-2 justify-start">
-                    <div className="text-white uppercase flex items-center justify-center min-w-6 w-6 h-6 rounded-sm bg-gradient-to-br from-purple-700 to-sky-300">
-                      {project.name.at(0)}
+                <AccordionTrigger className="text-white bg-transparent border-zinc-700 py-3 px-3 rounded-tl-md rounded-tr-md hover:*:no-underline">
+                  <div className="flex items-center gap-2 justify-start w-full">
+                    <div className="mr-auto flex gap-2">
+                      <div className="text-white uppercase flex items-center justify-center min-w-6 w-6 h-6 rounded-sm bg-sky-600">
+                        {project.name.at(0)}
+                      </div>
+                      <span className="capitalize text-white text-lg w-32 truncate">
+                        {project.name}
+                      </span>
                     </div>
-                    <span className="capitalize text-white text-lg">
-                      {project.name}
-                    </span>
+                    <div className="ml-auto flex gap-4 px-4">
+                      <div className="flex items-center space-x-2">
+                        <ClipboardList />
+                        <span className="bg-white p-2 text-black rounded-full h-6 w-6 text-sm font-bold flex items-center leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                          {project.tasks.length + 1}
+                        </span>
+                      </div>
+                    </div>
                   </div>
                 </AccordionTrigger>
-                <AccordionContent className="text-sm flex flex-col gap-3 px-3">
+                <AccordionContent className="data-[state=open]:mb-2 text-sm flex flex-col gap-3 px-3">
                   {project?.tasks.map((task, id) => {
                     return (
                       <SortableContext
